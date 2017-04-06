c9d88c9b6ba44acd4cc93ef6e4590f851ca262eb
vilasitha <vilasita.gavkar@gmail.com>
2017-04-06 14:32:56 -0500

    Second commit

---
+++ src/com/core/practice/MyTestCollection.java
@@ -0,0 +1,10 @@
+package com.core.practice;
+
+public class MyTestCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My collection test");
+	}
+
+}
